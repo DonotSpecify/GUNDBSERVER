249fec941bb76e91d5625851122fa0665407c907
DonotSpecify <2973352627@qq.com>
2024-12-11 20:25:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const GUN = require('gun');
-const server = require('http').createServer().listen(process.env.PORT || 8765);
+const server = require('https').createServer().listen(process.env.PORT || 8765);
 
 const gun = GUN({
   web: server
